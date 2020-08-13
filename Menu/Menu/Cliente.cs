35a17fa390862ad9dddef6cba9262be9c240a49e
lucresmac <kmlucasrm0@hotmail.com>
2020-08-12 22:11:43 -0300

    ok

---
+++ Menu/Menu/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace Menu
+{
+    class Cliente
+    {
+        //Atributo , propriedades  e caracteristicas
+        public Cliente()
+        {
+            CriandoEM = DateTime.Now;
+        }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime CriandoEM {get; set;}
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome} | CPF: {Cpf} | Criando em: {CriandoEM}";
+        }
+
+    }
+}
--- Menu/Menu/Program.cs
+++ Menu/Menu/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Menu
 {
@@ -6,11 +8,18 @@ namespace Menu
     {
         static void Main(string[] args)
         {
-            int op = -1;
-
-            while (op != 0) {
+            int op;
+            
+            //Criar lista
+            Cliente c = new Cliente();
+            List<Cliente> clientes = new List<Cliente>();
+            do
+            {
+                
+                         
+                // Limpar tela
                 Console.Clear();
-                Console.WriteLine( "======================================="
+                Console.WriteLine("======================================="
                                + "\n============     MENU     ============="
                                + "\n======================================="
                                + "\n==== 1 - Cadastrar Cliente        ====="
@@ -22,20 +31,61 @@ namespace Menu
                                + "\n==== 7 - Registrar Venda          ====="
                                + "\n==== 8 - Listar Vendas            ====="
                                + "\n==== 9 - Listar Vendas p/ Cliente ====="
-                               + "\n==== 1 - Cadastrar Cliente        ====="
+                               + "\n==== 0 - Sair                     ====="
                                + "\n=======================================");
                 Console.WriteLine("Digite sua opção: ");
                 op = Convert.ToInt16(Console.ReadLine());
                 Console.Clear();
 
-                    switch (op)
-                    {
+                switch (op)
+                {
                     case 1:
+                        //Para não dar problema na hora de listar na memoria
+                        c = new Cliente();
+                        // Colocar dados
                         Console.WriteLine("==== 1 - Cadastrar Cliente =====");
+                        Console.WriteLine("==== Digite nome do cliente =====");
+                        c.Nome = Console.ReadLine();
+                        Console.WriteLine("==== Digite CPF do cliente =====");
+                        c.Cpf = Console.ReadLine();
 
+                        //verificação de cpf
+                        //int teste = Convert.ToInt16(clientes); 
+                        if (clientes.Count == 0)
+                        {
+                            clientes.Add(c);
+                            Console.WriteLine("Primeiro cliente salvo com sucesso");
+                        }
+                        else
+                        {
+                            bool verific = false;
+                            foreach (Cliente clienteCadastrado in clientes)
+                            {
+                                if (clienteCadastrado.Cpf == c.Cpf)
+                                {
+                                    verific = true;
+                                }                              
+                            }
+                            if (verific)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("CPF já cadastrado , voltando ao menu");
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                clientes.Add(c);
+                                Console.WriteLine("Cliente salvo com sucesso");
+                                
+                            }
+                        }
                         break;
                     case 2:
-                        Console.WriteLine("==== 1 - Cadastrar Cliente =====");
+                        Console.WriteLine("==== 2 - List de Clientes =====");
+                        foreach (Cliente clienteCadastrado in clientes)
+                        {
+                            Console.WriteLine(clienteCadastrado);
+                        }
                         break;
                     case 0:
                         Console.WriteLine("====       Saindo...       =====");
@@ -43,12 +93,13 @@ namespace Menu
                     default:
                         Console.WriteLine("Opção invalida");
                         break;
-                    }
-                Console.WriteLine(        "\n==== Pressione uma tecla  =====");
+                }
+                Console.WriteLine("\n==== Pressione uma tecla  =====");
+                // Travar esperando opçao
                 Console.ReadKey();
 
 
-            }
+            } while (op != 0);
         }
     }
 }
